77103ffbfc32fb66941781ef328b965629b5e266
Lucas Lafèré <laferelucas@gmail.com>
2022-05-18 14:17:12 -0300

    adicionado alt pra img do footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer ({img, title}) {
     return (
         <div className="footer">
             <div className="footer-img-box">
-                <img src={img} alt="" />
+                <img src={img} alt={title} />
             </div>
             <h3>{title}</h3>
         </div>
