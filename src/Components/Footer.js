09d474b248a2f865e43072b14d5951332151b34e
Lucas Lafèré <laferelucas@gmail.com>
2022-05-19 13:18:33 -0300

    add funcionalidade de saber se tá disponivel ou nao

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,11 @@
-export default function Footer ({img, title}) {
+export default function Footer ({img, title, hour, day}) {
     return (
         <div className="footer">
             <div className="footer-img-box">
                 <img src={img} alt={title} />
             </div>
             <h3>{title}</h3>
+            <span>{day} - {hour}</span>
         </div>
     )
 }
--- src/Components/SelectSpot.js
+++ src/Components/SelectSpot.js
@@ -11,6 +11,7 @@ export default function SelectSpot () {
     const [seatsData, setSeatsData] = useState({});
     const [seats, setSeats] = useState([]);
     const [error, setError] = useState(false);
+    const [isLoading, setisLoading] = useState(true);
 
 
     useEffect(() => {
@@ -19,41 +20,60 @@ export default function SelectSpot () {
         promise.then(response => {
             setSeatsData(response.data);
             setSeats(response.data.seats)
+            setisLoading(false)
         })
         .catch(res => {
             setError(true);
         })
     }, []);
     console.log(seats)
+    console.log(seatsData)
+
+    if (isLoading) {
+        return "Aguarde um momento"
+    }
     return (
         <>
             {!error ? null : "deu ruim amigao"}
             <div className="container-seats">
                 <div className="box-h2">Selecione o(s) assento(s)</div>
                 <div className="box-seats">
-                    {seats.map(info => <Seat name={info.name} />)}
+                    {seats.map((info, index) => <Seat name={info.name} key={index} available={info.isAvailable}/>)}
                 </div>
                 <div className="captions-seats">
-                    <div className="seat"></div>
-                    <div className="seat"></div>
-                    <div className="seat"></div>
+                    <div className="seat-option">
+                        <div className="seat selected"></div>
+                        <span>Selecionado</span>
+                    </div>
+                    <div className="seat-option">
+                        <div className="seat"></div>
+                        <span>Disponível</span>
+                    </div>
+                    <div className="seat-option">
+                        <div className="seat unavailable"></div>
+                        <span>Indisponível</span>
+                    </div>
                 </div>
                 <div className="input-box">
-                    <input type="text" />
+                    <h4>Nome do comprador:</h4>
+                    <input type="text" placeholder='Digite seu nome...' />
                 </div>
                 <div className="input-box">
-                    <input type="text" />
+                    <h4>CPF do comprador:</h4>
+                    <input type="text" placeholder='Digite seu CPF...' />
                 </div>
-                <div className="order-button"></div>
+                <div className="order-button">
+                    <button className='order'>Reservar Assento(s)</button>
+                </div>
             </div>
-            <Footer />
+            {seatsData.length === 0 ? "Carregando, aguarde um instante" : <Footer hour={seatsData.name} day={seatsData.day.weekday} title={seatsData.movie.title} img={seatsData.movie.posterURL} />}
         </>
     )
 
 }
 
-function Seat ({name}) {
+function Seat ({name, available}) {
     return (
-        <div className="seat"><span>{name}</span></div>
+        <div className={available ? "seat" : "seat unavailable"}><span>{name}</span></div>
     )
 }
